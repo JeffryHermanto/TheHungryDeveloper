9a0ead25339401ffe73ca7e87fcdf3018da0cb2f
Jeffry Hermanto <jeffryhermanto@gmail.com>
2021-11-08 20:43:06 +0700

    Add desserts and show it's list

--- app/src/main/java/com/jeffryhermanto/thehungrydeveloper/MainActivity.java
+++ app/src/main/java/com/jeffryhermanto/thehungrydeveloper/MainActivity.java
@@ -5,12 +5,11 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     CardView startersCard;
     CardView mainsCard;
-
+    CardView dessertsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,21 +18,21 @@ public class MainActivity extends AppCompatActivity {
 
         startersCard = findViewById(R.id.card_view_starters);
         mainsCard = findViewById(R.id.card_view_mains);
+        dessertsCard = findViewById(R.id.card_view_desserts);
 
-        startersCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
-                startActivity(startersActivityIntent);
-            }
+        startersCard.setOnClickListener(view -> {
+            Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
+            startActivity(startersActivityIntent);
         });
 
-        mainsCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mainCoursesActivityIntent = new Intent(MainActivity.this, MainCoursesActivity.class);
-                startActivity(mainCoursesActivityIntent);
-            }
+        mainsCard.setOnClickListener(view -> {
+            Intent mainCoursesActivityIntent = new Intent(MainActivity.this, MainCoursesActivity.class);
+            startActivity(mainCoursesActivityIntent);
+        });
+
+        dessertsCard.setOnClickListener(view -> {
+            Intent dessertsActivityIntent = new Intent(MainActivity.this, DessertsActivity.class);
+            startActivity(dessertsActivityIntent);
         });
     }
 }
